c422ce8f85bcb4621c35c6289fc5c97dac311e9d
DmitryBush <bushuev0458@gmail.com>
2024-02-03 00:59:14 +0300

    Added rotation to tracks

--- Warrior's Soul/Assets/Scripts/For_Player/Spawn_Tracks.cs
+++ Warrior's Soul/Assets/Scripts/For_Player/Spawn_Tracks.cs
@@ -8,16 +8,10 @@ public class Spawn_Tracks : MonoBehaviour
 {
     public GameObject SpawnTracks;
     public GameObject Track;
-    //private Player.ViewSide viewSide;
     public Player player;
 
-    //Player.ViewSide viewSide;
-    int cooldown=0;
-    bool can_track=true;
-    //Vector3 pos;
-
-    int cooldown_track = 0;
-    //Vector3 cur_pos;
+    private bool can_track=true;
+    private int cooldown_track = 0;
 
     private void Update()
     {
@@ -32,59 +26,58 @@ public class Spawn_Tracks : MonoBehaviour
         }
     }
 
-    //private void OnTriggerStay2D(Collider2D collision)
-    //{
-    //    if (can_track == false)
-    //    {
-    //        cooldown++;
-    //        if(cooldown >= 1200*Time.deltaTime)
-    //        {
-    //            cooldown = 0;
-    //            can_track = true;
-    //        }
-    //    }
-    //}
-
     private void OnTriggerStay2D(Collider2D collision)
     {
-        //if (collision.transform.tag == "Sand_Surface" && can_track == true)
-        //{
-        //    can_track = false;
-        //    viewSide = player.viewSide;
-        //    pos = new Vector3(SpawnTracks.transform.position.x, SpawnTracks.transform.position.y, 1f);
-        //    if (viewSide == Player.ViewSide.Down_Left || viewSide == Player.ViewSide.Up_Left || viewSide == Player.ViewSide.Left)
-        //    {
-        //        Instantiate(Track, pos, transform.rotation);
-        //    }
-        //Debug.Log("Rdfsdg");
-        if (collision.transform.tag == "Sand" && can_track==true)
+        if (collision.transform.tag == "Sand" && can_track == true)
         {
-            Debug.Log("Rdfsdg");
             can_track = false;
             var viewSide = player.viewSide;
             var cur_pos = new Vector3(SpawnTracks.transform.position.x, SpawnTracks.transform.position.y, 1f);
-            
-            if (viewSide == Player.ViewSide.Down_Left || viewSide == Player.ViewSide.Up_Left 
-                || viewSide == Player.ViewSide.Left)
-            {
-                //Vector3 rotate = new Vector3(0, 0, 90);
-                Instantiate(Track, cur_pos, transform.rotation);
-            }
 
-            else if (viewSide == Player.ViewSide.Down_Right || viewSide == Player.ViewSide.Up_Right 
-                || viewSide == Player.ViewSide.Right)
+            //if (viewSide == Player.ViewSide.Down_Left || viewSide == Player.ViewSide.Up_Left 
+            //    || viewSide == Player.ViewSide.Left)
+            //{
+            //    Instantiate(Track, cur_pos, Quaternion.Euler(new Vector3(0, 0, 180)));
+            //}
+            //else if (viewSide == Player.ViewSide.Down_Right || viewSide == Player.ViewSide.Up_Right 
+            //    || viewSide == Player.ViewSide.Right)
+            //{
+            //    Instantiate(Track, cur_pos, Quaternion.Euler(new Vector3(0, 0, 0)));
+            //}
+            //else if (viewSide == Player.ViewSide.OnScreen) // от нас
+            //{
+            //    Instantiate(Track, cur_pos, Quaternion.Euler(new Vector3(0, 0, 90)));
+            //}
+            //else if (viewSide == Player.ViewSide.OnMe)   // к нам
+            //{
+            //    Instantiate(Track, cur_pos, Quaternion.Euler(new Vector3(0, 0, 270)));
+            //}
+            switch (viewSide)
             {
-                Instantiate(Track, cur_pos, transform.rotation);
-            }
-
-            else if (viewSide == Player.ViewSide.OnScreen) // от нас
-            {
-                Instantiate(Track, cur_pos, transform.rotation);
-            }
-
-            else if (viewSide == Player.ViewSide.OnMe)   // к нам
-            {
-                Instantiate(Track, cur_pos, transform.rotation);
+                case Player.ViewSide.Left:
+                    Instantiate(Track, cur_pos, Quaternion.Euler(new Vector3(0, 0, 180)));
+                    break;
+                case Player.ViewSide.Right:
+                    Instantiate(Track, cur_pos, Quaternion.Euler(new Vector3(0, 0, 180)));
+                    break;
+                case Player.ViewSide.OnScreen:
+                    Instantiate(Track, cur_pos, Quaternion.Euler(new Vector3(0, 0, 90)));
+                    break;
+                case Player.ViewSide.OnMe:
+                    Instantiate(Track, cur_pos, Quaternion.Euler(new Vector3(0, 0, 270)));
+                    break;
+                case Player.ViewSide.Down_Right:
+                    Instantiate(Track, cur_pos, Quaternion.Euler(new Vector3(0, 0, 315)));
+                    break;
+                case Player.ViewSide.Up_Right:
+                    Instantiate(Track, cur_pos, Quaternion.Euler(new Vector3(0, 0, 45)));
+                    break;
+                case Player.ViewSide.Up_Left:
+                    Instantiate(Track, cur_pos, Quaternion.Euler(new Vector3(0, 0, 135)));
+                    break;
+                case Player.ViewSide.Down_Left:
+                    Instantiate(Track, cur_pos, Quaternion.Euler(new Vector3(0, 0, 225)));
+                    break;
             }
         }
     }
