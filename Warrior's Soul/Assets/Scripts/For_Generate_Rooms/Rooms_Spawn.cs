a93e08ee59a4cf582ff9412dc4e19de270750247
Cheremin Dmitriy <126575364+dmitriychearemin@users.noreply.github.com>
2024-02-06 13:16:51 +0300

    ScriptsFor_generate+ Ne Generiruet_Need_Rework

---
+++ Warrior's Soul/Assets/Scripts/For_Generate_Rooms/Rooms_Spawn.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class Rooms_Spawn : MonoBehaviour
+{
+
+    public Direction direction;
+
+    public enum Direction
+    {
+        Up,
+        Down,
+        Right,
+        Left
+    }
+
+    private Rooms_Variants variants;
+    private bool Can_Spawn = true;
+    private int rand;
+    private float waitTime = 3f;
+
+    private void Start()
+    {
+        variants = GameObject.FindGameObjectWithTag("Room").GetComponent<Rooms_Variants>();
+        Destroy(gameObject,waitTime);
+        Invoke("Spawn_Room", 0.2f);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Spawn_Room()
+    {
+        if (Can_Spawn==true) {
+            if(direction == Direction.Up)
+            {
+                rand = Random.Range(0, variants.Rooms_Up.Length);
+                Instantiate(variants.Rooms_Up[rand], transform.position, variants.Rooms_Up[rand].transform.rotation);            
+            }
+
+            else if (direction == Direction.Down)
+            {
+                rand = Random.Range(0, variants.Rooms_Down.Length);
+                Instantiate(variants.Rooms_Down[rand], transform.position, variants.Rooms_Down[rand].transform.rotation);
+
+            }
+
+            else if (direction == Direction.Left)
+            {
+                rand = Random.Range(0, variants.Rooms_Left.Length);
+                Instantiate(variants.Rooms_Left[rand], transform.position, variants.Rooms_Left[rand].transform.rotation);
+
+            }
+
+            else if (direction == Direction.Right)
+            {
+                rand = Random.Range(0, variants.Rooms_Right.Length);
+                Instantiate(variants.Rooms_Right[rand], transform.position, variants.Rooms_Right[rand].transform.rotation);
+
+            }
+            Can_Spawn = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Point_Spawn_Room") && collision.GetComponent<Rooms_Spawn>().Can_Spawn)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Warrior's Soul/Assets/Scripts/For_Generate_Rooms/Rooms_Variants.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rooms_Variants : MonoBehaviour
+{
+
+    public GameObject[] Rooms_Down;
+    public GameObject[] Rooms_Up;
+    public GameObject[] Rooms_Left;
+    public GameObject[] Rooms_Right;
+}
