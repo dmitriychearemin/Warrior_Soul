0f508b110fa5df27ea3a89abb082a7e609998b96
Dmitry Bush <bushuev0458@gmail.com>
2024-02-06 21:39:56 +0300

    Attack mouse controll

--- Warrior's Soul/Assets/Scripts/For_Player/Keyboard_control.cs
+++ Warrior's Soul/Assets/Scripts/For_Player/Keyboard_control.cs
@@ -7,6 +7,8 @@ public class Keyboard_control : MonoBehaviour
 {
     //public 
     Player player;
+    float halfScreenX = Screen.width / 2, halfScreenY = Screen.height / 2;
+    float thirdScreenX = Screen.width / 3, thirdScreenY = Screen.height / 3;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,39 @@ public class Keyboard_control : MonoBehaviour
     {
         if(HitPoint.GetStamina() > 0)
         {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var mousePos = Input.mousePosition;
+                switch (GetScreenArea(Input.mousePosition)) 
+                {
+                    case Player.ViewSide.Up_Left:
+                        player.Attack_Up_Left();
+                        break;
+                    case Player.ViewSide.Up_Right:
+                        player.Attack_Up_Right();
+                        break;
+                    case Player.ViewSide.OnMe:
+                        player.Attack_Up(); 
+                        break;
+                    case Player.ViewSide.Left:
+                        player.Attack_Left();
+                        break;
+                    case Player.ViewSide.Right:
+                        player.Attack_Right();
+                        break;
+                    case Player.ViewSide.Down_Left: 
+                        player.Attack_Down_Left();
+                        break;
+                    case Player.ViewSide.Down_Right:
+                        player.Attack_Down_Right();
+                        break;
+                    case Player.ViewSide.OnScreen:
+                        player.Attack_Down();
+                        break;
+                    default:
+                        break;
+                }
+            }
             if (Input.GetKey(KeyCode.Delete))
                 player.DeadAnimation();
             //////////////////////////////////////////////
@@ -182,5 +217,51 @@ public class Keyboard_control : MonoBehaviour
                 player.Walk_Move_Right();
             }
         }
+    }
+
+    private Player.ViewSide GetScreenArea(Vector3 mousePos)
+    {
+        if(mousePos.x > 0 && mousePos.x < Screen.width 
+            && mousePos.y > 0 && mousePos.y < Screen.height)
+        {
+            if (mousePos.x <= halfScreenX && mousePos.y > halfScreenY)
+            {
+                if (mousePos.x <= thirdScreenX && mousePos.y > thirdScreenY)
+                    return Player.ViewSide.Up_Left;
+                else if (mousePos.y <= thirdScreenY)
+                    return Player.ViewSide.Left;
+                else
+                    return Player.ViewSide.OnMe;
+            }
+            else if (mousePos.x > halfScreenX && mousePos.y > halfScreenY)
+            {
+                if (mousePos.x > thirdScreenX * 2 && mousePos.y > thirdScreenY)
+                    return Player.ViewSide.Up_Right;
+                else if (mousePos.y <= thirdScreenY)
+                    return Player.ViewSide.Right;
+                else
+                    return Player.ViewSide.OnMe;
+            }
+            else if (mousePos.x <= halfScreenX && mousePos.y <= halfScreenY)
+            {
+                if (mousePos.x <= thirdScreenX && mousePos.y <= thirdScreenY * 2)
+                    return Player.ViewSide.Down_Left;
+                else if (mousePos.y > thirdScreenY * 2)
+                    return Player.ViewSide.Left;
+                else
+                    return Player.ViewSide.OnScreen;
+            }
+            else
+            {
+                if (mousePos.x > thirdScreenX * 2 && mousePos.y <= thirdScreenY * 2)
+                    return Player.ViewSide.Down_Right;
+                else if (mousePos.y > thirdScreenY * 2)
+                    return Player.ViewSide.Right;
+                else
+                    return Player.ViewSide.OnScreen;
+            }
+        }
+        else
+            return (Player.ViewSide)8;
     }
 }
