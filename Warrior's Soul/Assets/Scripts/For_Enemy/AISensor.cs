9eff02511f9a6ad4cd5f434f4024915a8d433423
Dmitry Bush <bushuev0458@gmail.com>
2024-04-04 19:44:42 +0300

    raycast added

--- Warrior's Soul/Assets/Scripts/For_Enemy/AISensor.cs
+++ Warrior's Soul/Assets/Scripts/For_Enemy/AISensor.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 
 public class AISensor : MonoBehaviour
 {
-    [SerializeField] private float distance = 10;
-    [SerializeField] private float angle = 30;
+    [field: SerializeField] public float Distance { get; private set; } = 10;
+    [field: SerializeField] public float Angle { get; private set; } = 30;
     [SerializeField] private Color color = Color.red;
 
-    private Mesh mesh;
+    public List<Transform> Targets { get; private set; } = new();
+
+    public LayerMask targetMask, obstacleMask;
+    private float scanInterval = 0.2f;
+    private float scanTimer = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -20,85 +24,119 @@ public class AISensor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    Mesh CreateSprite()
-    {
-        Mesh mesh = new();
-        int numTriangles = 8;
-        int numVertices = numTriangles * 3;
-
-        Vector3[] vertices = new Vector3[numVertices];
-        int[] triangles = new int[numVertices];
-
-        Vector3 bottomCenter = Vector3.zero;
-        Vector3 bottomLeft = Quaternion.Euler(0, 0, -angle) * Vector3.up * distance;
-        Vector3 bottomRight = Quaternion.Euler(0, 0, angle) * Vector3.up * distance;
-
-        Vector3 topCenter = bottomCenter + Vector3.forward;
-        Vector3 topLeft = bottomLeft + Vector3.forward;
-        Vector3 topRight = bottomRight + Vector3.forward;
-        
-        int vert = 0;
-
-        vertices[vert++] = bottomCenter;
-        vertices[vert++] = bottomLeft;
-        vertices[vert++] = topLeft;
-
-        vertices[vert++] = topLeft;
-        vertices[vert++] = topCenter;
-        vertices[vert++] = bottomCenter;
-
-        // right
-        vertices[vert++] = bottomCenter;
-        vertices[vert++] = topCenter;
-        vertices[vert++] = topRight;
-
-        vertices[vert++] = topRight;
-        vertices[vert++] = bottomRight;
-        vertices[vert++] = bottomCenter;
-
-        // far
-        vertices[vert++] = bottomLeft;
-        vertices[vert++] = bottomRight;
-        vertices[vert++] = topRight;
-
-        vertices[vert++] = topRight;
-        vertices[vert++] = topLeft;
-        vertices[vert++] = bottomLeft;
-
-        // top
-        vertices[vert++] = topCenter;
-        vertices[vert++] = topLeft;
-        vertices[vert++] = topRight;
-
-        // bottom
-        vertices[vert++] = bottomCenter;
-        vertices[vert++] = bottomRight;
-        vertices[vert++] = bottomLeft;
-
-        for (int i = 0; i < numVertices; i++)
-            triangles[i] = i;
-
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        mesh.RecalculateNormals();
-
-        return mesh;
-    }
-
-    private void OnValidate()
-    {
-        mesh = CreateSprite();
-    }
-
-    private void OnDrawGizmos()
-    {
-        if (mesh)
+        scanTimer -= Time.deltaTime;
+        if (scanTimer <= 0) 
         {
-            Gizmos.color = color;
-            Gizmos.DrawMesh(mesh, transform.position, transform.rotation);
+            scanTimer = 0.2f;
+            Scan();
         }
+    }
+
+    void Scan()
+    {
+        Targets.Clear();
+        Collider[] targetsInView = Physics.OverlapSphere(transform.position,
+            Distance, targetMask);
+
+        for (int i = 0; i < targetsInView.Length; i++)
+        {
+            Transform target = targetsInView[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < Angle / 2)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                {
+                    Targets.Add(target);
+                }
+            }
+        }
+    }
+
+    //Mesh CreateSprite()
+    //{
+    //    Mesh mesh = new();
+    //    int numTriangles = 8;
+    //    int numVertices = numTriangles * 3;
+
+    //    Vector3[] vertices = new Vector3[numVertices];
+    //    int[] triangles = new int[numVertices];
+
+    //    Vector3 bottomCenter = Vector3.zero;
+    //    Vector3 bottomLeft = Quaternion.Euler(0, 0, -angle) * Vector3.up * distance;
+    //    Vector3 bottomRight = Quaternion.Euler(0, 0, angle) * Vector3.up * distance;
+
+    //    Vector3 topCenter = bottomCenter + Vector3.forward;
+    //    Vector3 topLeft = bottomLeft + Vector3.forward;
+    //    Vector3 topRight = bottomRight + Vector3.forward;
+
+    //    int vert = 0;
+
+    //    vertices[vert++] = bottomCenter;
+    //    vertices[vert++] = bottomLeft;
+    //    vertices[vert++] = topLeft;
+
+    //    vertices[vert++] = topLeft;
+    //    vertices[vert++] = topCenter;
+    //    vertices[vert++] = bottomCenter;
+
+    //    // right
+    //    vertices[vert++] = bottomCenter;
+    //    vertices[vert++] = topCenter;
+    //    vertices[vert++] = topRight;
+
+    //    vertices[vert++] = topRight;
+    //    vertices[vert++] = bottomRight;
+    //    vertices[vert++] = bottomCenter;
+
+    //    // far
+    //    vertices[vert++] = bottomLeft;
+    //    vertices[vert++] = bottomRight;
+    //    vertices[vert++] = topRight;
+
+    //    vertices[vert++] = topRight;
+    //    vertices[vert++] = topLeft;
+    //    vertices[vert++] = bottomLeft;
+
+    //    // top
+    //    vertices[vert++] = topCenter;
+    //    vertices[vert++] = topLeft;
+    //    vertices[vert++] = topRight;
+
+    //    // bottom
+    //    vertices[vert++] = bottomCenter;
+    //    vertices[vert++] = bottomRight;
+    //    vertices[vert++] = bottomLeft;
+
+    //    for (int i = 0; i < numVertices; i++)
+    //        triangles[i] = i;
+
+    //    mesh.vertices = vertices;
+    //    mesh.triangles = triangles;
+    //    mesh.RecalculateNormals();
+
+    //    return mesh;
+    //}
+
+    //private void OnValidate()
+    //{
+    //    mesh = CreateSprite();
+    //}
+
+    //private void OnDrawGizmos()
+    //{
+    //    Handles.color = color;
+    //    Handles.DrawWireArc()
+    //}
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 
+            0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
 }
